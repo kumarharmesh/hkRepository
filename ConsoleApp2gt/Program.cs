a9f214689abfd458e3a881f52f1cf993af79f84d
hk <kumarharmesh@gmail.com>
2018-10-23 16:19:58 +0530

    sqrt saved

---
+++ ConsoleApp2gt/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp2gt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double ans=Math.Sqrt(30);
+            Console.WriteLine(ans);
+        }
+    }
+}
