bdb61bc774a73aebc67aed9f5648d4fb9368f4f0
kumarharmesh <kumarharmesh@gmail.com>
2018-10-31 22:29:53 +0530

    Pool.cs file uploaded and added to project

---
+++ pool.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class pool
+    {
+        static void Main(string[] args)
+        {
+            WaitCallback cb = Run;
+            ThreadPool.QueueUserWorkItem(cb, 12);
+            Console.WriteLine(DateTime.Now.ToString());
+        }
+        public static void Run(object state)
+        {
+            int table = (int)state;
+            for(int i=1;i<=10;i++)
+                Console.WriteLine($"{table}X{i}={table*i}");
+        }
+    }
+}
