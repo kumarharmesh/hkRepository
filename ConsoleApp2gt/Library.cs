6847dbe966613d3eada865961ebc1fe61de9ed93
Harmesh Kumar <kumarharmesh@gmail.com>
2018-10-31 23:24:38 +0530

    Revert "Number and Root deleted"
    This reverts commit c88bcd49a35d6e13665999120f172925378c1048.

---
+++ ConsoleApp2gt/Library.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2gt
+{
+    class Library
+    {
+        public string GetDate()
+        {
+            return DateTime.Now.ToString();
+        }
+        public int Day()
+        {
+           return 1200;   
+        }
+        public double Root(int x)
+        {
+         return Math.Sqrt(x);   
+        }
+    }
+}
