03b4b39389816550b4cdd51866e4cf656d62fac3
Harmesh Kumar <kumarharmesh@gmail.com>
2018-10-31 23:10:56 +0530

    Library.cs added

---
+++ ConsoleApp2gt/Library.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2gt
+{
+    class Library
+    {
+        public string GetDate()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
