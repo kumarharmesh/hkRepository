ba15e0f6be05c6e40ff1fd698db6961bc78034de
Harmesh Kumar <kumarharmesh@gmail.com>
2018-11-03 10:19:11 +0530

    Dateexamples class added

---
+++ ConsoleApp2gt/Dateexamples.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2gt
+{
+    class Dateexamples
+    {
+        public static string Datetime()
+        {
+            DateTime d1= DateTime.Now;
+            DateTime d2 = DateTime.Now.AddDays(4);
+
+            var x = (d2-d1).Days;
+            return "";
+        }
+        static void Main(string[] args)
+        {
+            Dateexamples.Datetime();
+        }
+    }
+}
