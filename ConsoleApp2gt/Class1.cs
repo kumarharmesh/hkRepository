098163bc7071613def99b567a510bcd6a5ac6421
Harmesh Kumar <kumarharmesh@gmail.com>
2019-01-25 20:47:22 +0530

    class1.cs added

---
+++ ConsoleApp2gt/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2gt
+{
+    class Class1
+    {
+        public static double power(int x,int y)
+        {
+            return Math.Pow(x, y);
+        }
+    }
+}
